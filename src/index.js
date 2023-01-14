1eeaa0fc3ffae695b6ab8bc2663b10edb98f6bd8
Mikalai Alkhimenka <108357239+Mikalai-A@users.noreply.github.com>
2023-01-14 22:50:08 +0100

    feat: add solution reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let reversed = n.toString().split('').reverse().join('');
+
+    if (reversed.endsWith('-')) 
+        return parseInt(reversed);
+
+    return parseInt(reversed);
 }
